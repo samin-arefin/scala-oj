ef902b0b718950d90fe4ce24089ef686d41f83a0
samin-arefin <nimas.inn@gmail.com>
2016-03-23 08:10:08 +0600

    add tab character to the default delimiter and shorten the code

--- build.sbt
+++ build.sbt
@@ -3,4 +3,16 @@ name := "scala-oj"
 version := "1.0"
 
 scalaVersion := "2.11.7"
-    
+
+scalacOptions in ThisBuild ++= Seq("-deprecation", "-feature")
+
+libraryDependencies ++= Seq(
+  "org.scalanlp" %% "breeze" % "0.12",
+  "org.scalanlp" %% "breeze-natives" % "0.12",
+  "org.scalanlp" %% "breeze-viz" % "0.12",
+  "org.scalaz" %% "scalaz-core" % "7.2.1"
+)
+
+resolvers ++= Seq(
+  "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
+)
--- src/main/scala-2.11/template/IOTemplate.scala
+++ src/main/scala-2.11/template/IOTemplate.scala
@@ -11,51 +11,31 @@ import scala.reflect.ClassTag
 /**
   * Created by nimas on 12/24/15.
   */
-class IOTemplate(val delim: String = " ",
-                 val input: InputStream = System.in,
-                 val output: OutputStream = System.out) extends Iterator[String] {
+class IOTemplate(val delim: String = " \t",
+                 private val input: InputStream = System.in,
+                 private val output: OutputStream = System.out) extends Iterator[String] {
 
   val in  = new BufferedReader(new InputStreamReader(input))
   val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(output)))
 
   @implicitNotFound("No member of type class ParseAble in scope for ${T}")
-  trait ParseAble[T] {
-    def parse(strTok: StringTokenizer): T
-  }
+  trait ParseAble[T] { val parse: StringTokenizer => T }
 
   object ParseAble {
 
-    def apply[T](f: StringTokenizer => T) = new ParseAble[T] {
-      override def parse(strTok: StringTokenizer): T = f(strTok)
-    }
+    def apply[T](f: StringTokenizer => T) = new ParseAble[T] { override val parse: StringTokenizer => T = f(_) }
 
-    object ParseAbleLine extends ParseAble[String] {
-      override def parse(strTok: StringTokenizer): String = strTok nextToken "\n"
-    }
+    object ParseAbleLine extends ParseAble[String] { override val parse: StringTokenizer => String = _ nextToken "\n" }
 
-    implicit object ParseAbleString extends ParseAble[String] {
-      override def parse(strTok: StringTokenizer): String = strTok nextToken()
-    }
-
-    implicit object ParseAbleInt extends ParseAble[Int] {
-      override def parse(strTok: StringTokenizer): Int = strTok nextToken() toInt
-    }
-
-    implicit object ParseAbleLong extends ParseAble[Long] {
-      override def parse(strTok: StringTokenizer): Long = strTok nextToken() toLong
-    }
-
-    implicit object ParseAbleDouble extends ParseAble[Double] {
-      override def parse(strTok: StringTokenizer): Double = strTok nextToken() toDouble
-    }
-
-    implicit object ParseAbleBigInt extends ParseAble[BigInt] {
-      override def parse(strTok: StringTokenizer): BigInt = BigInt(strTok nextToken())
-    }
+    implicit val ParseAbleString = new ParseAble[String] { override val parse: (StringTokenizer) => String = _ nextToken() }
+    implicit val ParseAbleInt    = new ParseAble[Int]    { override val parse: (StringTokenizer) => Int = _ nextToken() toInt }
+    implicit val ParseAbleLong   = new ParseAble[Long]   { override val parse: (StringTokenizer) => Long = _ nextToken() toLong }
+    implicit val ParseAbleDouble = new ParseAble[Double] { override val parse: (StringTokenizer) => Double = _ nextToken() toDouble }
+    implicit val ParseAbleBigInt = new ParseAble[BigInt] { override val parse = (strTok: StringTokenizer) => BigInt(strTok nextToken()) }
 
   }
 
-  val it = Iterator.iterate(Option(new StringTokenizer(""))) {
+  private val it = Iterator.iterate(Option(new StringTokenizer(""))) {
     case Some(strTok) =>
       if (strTok.hasMoreTokens) Some(strTok)
       else {
@@ -66,7 +46,7 @@ class IOTemplate(val delim: String = " ",
     case _ => None
   }.buffered
 
-  def hasNext: Boolean = it.head match {
+  override def hasNext: Boolean = it.head match {
     case Some(tok) =>
       if (tok.hasMoreTokens) true
       else {
@@ -76,20 +56,18 @@ class IOTemplate(val delim: String = " ",
     case None => false
   }
 
-  def next = read[String]
-
-  def readLine = read(ParseAble.ParseAbleLine)
-
-  def readAll[T: ParseAble](n: Int)(implicit ev: ClassTag[T]) = (Array fill n) {
-    read[T]
-  }
+  override def next = read[String]
 
   def read[T](implicit parser: ParseAble[T]) = it
     .find {
       case Some(strTok) => strTok.hasMoreTokens
-      case None => throw new Exception("nothing found of such type")
+      case None => sys.error("nothing found of such type")
     }
     .collect { case Some(strTok) => parser parse strTok } get
+
+  def readLine = read(ParseAble.ParseAbleLine)
+
+  def readAll[T: ParseAble](n: Int)(implicit ev: ClassTag[T]) = (Array fill n) { read[T] }
 
   def close(): Unit = {
     in.close()
